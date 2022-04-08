7c35344466adce489534249fb80f82fab4c8f5b0
juancho <juanchoj1024@gmail.com>
2022-04-08 09:25:40 -0400

    pulled integrity comparison into a more meaningful place

--- src/test/scala/io/iog/minichain/models/MinerSpec.scala
+++ src/test/scala/io/iog/minichain/models/MinerSpec.scala
@@ -8,8 +8,10 @@ import zio.test.TestAspect.*
 object MinerSpec extends DefaultRunnableSpec:
   def spec = suite("miner spec")(
     test("multi-fiber mines") {
-      assertM(Task.foreachPar(0 until 100) { i =>
-        Miner.mine(i, Hash("hello".getBytes), Seq("2.1", "2.2"), Miner.StdMiningTargetNumber).map(_.minedProperly)
-      })(forall(isTrue))
+      Task.foreachPar(0 until 100) { i =>
+        Miner.mine(i, Hash("hello".getBytes), Seq("2.1", "2.2"), Miner.StdMiningTargetNumber)
+      }.map { blocks =>
+        assertTrue(blocks.forall(_.minedProperly))
+      }
     } @@ nonFlaky(5)
   )
